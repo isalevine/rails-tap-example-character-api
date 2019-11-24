3e7229c952a538a88b78535bd7ab2c4c23369658
isalevine <isa.loves.coding@gmail.com>
2019-11-24 13:39:41 -0800

    refactored to roll all stats at once, keeping track of remaining stats and allocating appropriately; implemented debugging with .tap (ORGANICALLY); added Rspec tests for the stat point allocation, and Character database count

--- app/services/random_character_generator.rb
+++ app/services/random_character_generator.rb
@@ -1,36 +1,103 @@
+# BEFORE:
+# ==============================================================
+
+# class RandomCharacterGenerator
+#     def initialize
+#     end
+
+#     def new_character(name)
+#         character = Character.new(name: name)
+#         points_pool = 9
+
+#         roll_stat(character, "strength", points_pool)
+#         roll_stat(character, "dexterity", points_pool)
+#         roll_stat(character, "intelligence", points_pool)
+#         roll_stat(character, "charisma", points_pool)
+#         # check for leftover points in points_pool
+
+#         character
+
+
+#         # alt using .tap:
+#         # =====================================================
+#         # Character.new.tap do |character|
+#         #     points_pool = 9
+#         #     roll_stat(character, "strength", points_pool)
+#         #     roll_stat(character, "dexterity", points_pool)
+#         #     roll_stat(character, "intelligence", points_pool)
+#         #     roll_stat(character, "charisma", points_pool)
+#         # end
+#     end
+
+#     def roll_stat(character, stat, points_pool)
+#         roll = rand(1..6)
+#         if points_pool - roll < 0
+#             character[stat] = points_pool
+#         # check if points_pool will be 0 or less here??
+#         character[stat] = roll
+#         points_pool -= roll
+#     end
+
+#     def allocate_extra_points(character, points_pool)
+#         # allocate extra points to highest stat?
+#     end
+
+# end
+
+
+
+
+
+
+
+# AFTER:
+# ==============================================================
+
 class RandomCharacterGenerator
     def initialize
     end
 
     def new_character(name)
         character = Character.new(name: name)
-        points_pool = 20
-
-        roll_stat(character, "strength", points_pool)
-        roll_stat(character, "dexterity", points_pool)
-        roll_stat(character, "intelligence", points_pool)
-        roll_stat(character, "charisma", points_pool)
-        # check for leftover points in points_pool
-
+        stats_array = ["strength", "dexterity", "intelligence", "charisma"]
+        points_pool = 9
+        roll_stats(character, stats_array, points_pool)
         character
 
 
         # alt using .tap:
         # =====================================================
         # Character.new.tap do |character|
-        #     points_pool = 20
-        #     roll_stat(character, "strength", points_pool)
-        #     roll_stat(character, "dexterity", points_pool)
-        #     roll_stat(character, "intelligence", points_pool)
-        #     roll_stat(character, "charisma", points_pool)
+        #     stats_array = ["strength", "dexterity", "intelligence", "charisma"]
+        #     points_pool = 9
+        #     roll_stats(character, stats_array, points_pool)
         # end
     end
 
-    def roll_stat(character, stat, points_pool)
-        roll = rand(1..6)
-        # check if points_pool will be 0 or less here??
-        character[stat] = roll
-        points_pool -= roll
+
+    private
+
+    def roll_stats(character, stats_array, points_pool)
+        stats_array.each_with_index do |stat, index|
+            roll = rand(1..6)                                           .tap {|r| puts "roll: #{r}"}
+            remaining_stats = ((stats_array.length - 1) - index)        .tap {|r| puts "remaining_stats: #{r}"}
+                                                                        .tap {|r| puts "points_pool (before): #{points_pool}"}
+            if remaining_stats == 0
+                character[stat] = points_pool
+                points_pool = 0
+            elsif points_pool - roll < remaining_stats
+                max_points = points_pool - remaining_stats
+                character[stat] = max_points
+                points_pool -= max_points
+            elsif points_pool - roll < 0        # same behavior as first if condition -- not DRY...
+                character[stat] = points_pool
+                points_pool = 0
+            else
+                character[stat] = roll
+                points_pool -= roll
+            end                                                         .tap {|r| puts "character[#{stat}]: #{character[stat]}"}
+                                                                        .tap {|r| puts "points_pool (after): #{points_pool}\n\n"}
+        end
     end
 
     def allocate_extra_points(character, points_pool)
--- spec/services/random_character_generator_spec.rb
+++ spec/services/random_character_generator_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe RandomCharacterGenerator do
 
     describe "#def new_character(name)" do
-        # use a before_each block on this?
+        starting_database_count = Character.count
+        stat_points = 9
+        stat_max = 6        # highest value of RNG, ln.82: roll = rand(1..6)
+
         random_character_generator = RandomCharacterGenerator.new
         character = random_character_generator.new_character("Mac")
 
@@ -11,11 +14,19 @@ RSpec.describe RandomCharacterGenerator do
             expect(character).to be_an_instance_of Character
         end
 
-        it "randomly allocates 20 stat points between Strength, Dexterity, Intelligence, and Charisma" do
-            expect(character.strength + character.dexterity + character.intelligence + character.charisma).to eq 20
+        it "randomly allocates all #{stat_points} stat points between Strength, Dexterity, Intelligence, and Charisma" do
+            expect(character.strength + character.dexterity + character.intelligence + character.charisma).to eq stat_points
+        end
+
+        it "allocates stat points so they do not exceed maximum amount (#{stat_max})" do
+            expect(character.strength).to be_between(1, stat_max)
+            expect(character.dexterity).to be_between(1, stat_max)
+            expect(character.intelligence).to be_between(1, stat_max)
+            expect(character.charisma).to be_between(1, stat_max)
         end
 
         it "saves the Character to the database" do
+            expect(Character.count).to eq (starting_database_count + 1)
         end
     end
     
