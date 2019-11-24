9ea0061e868c02b2498e2cc059cf94835d0c89e7
isalevine <isa.loves.coding@gmail.com>
2019-11-24 15:09:17 -0800

    added in pre-refactor Rspec tests with hard-coded stat_points and stat_max variables, to use with future Rspec dev.to blog post

--- spec/services/random_character_generator_spec.rb
+++ spec/services/random_character_generator_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
+
+# BEFORE: (hard-coding stat_points and stat_max to match same hard-coded values in rcg service object)
+# ====================================================================================================
+# RSpec.describe RandomCharacterGenerator do
+
+#     describe "#def new_character(name)" do
+#         starting_database_count = Character.count
+
+#         random_character_generator = RandomCharacterGenerator.new
+#         character = random_character_generator.new_character("Mac")
+
+#         stat_points = 9
+#         stat_max = 6        # highest value of RNG, ln.82: roll = rand(1..6)
+
+#         it "creates a new Character instance" do
+#             expect(character).to be_an_instance_of Character
+#         end
+
+#         it "randomly allocates all #{stat_points} stat points between Strength, Dexterity, Intelligence, and Charisma" do
+#             expect(character.strength + character.dexterity + character.intelligence + character.charisma).to eq stat_points
+#         end
+
+#         it "allocates stat points so they do not exceed maximum amount (#{stat_max})" do
+#             expect(character.strength).to be_between(1, stat_max)
+#             expect(character.dexterity).to be_between(1, stat_max)
+#             expect(character.intelligence).to be_between(1, stat_max)
+#             expect(character.charisma).to be_between(1, stat_max)
+#         end
+
+#         it "saves the Character to the database" do
+#             expect(Character.count).to eq (starting_database_count + 1)
+#         end
+#     end
+    
+# end
+
+
+
+# AFTER:
+# ====================================================================================================
 RSpec.describe RandomCharacterGenerator do
 
     describe "#new_character(name)" do
